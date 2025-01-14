a1df2599d0f6170efcfc39844bcebda1d7c85ed1
Suraj Malinga <malinga91@gmail.com>
2025-01-15 00:43:35 +0530

    added reading time for articles

---
+++ scripts/content.js
@@ -0,0 +1,32 @@
+const article = document.querySelector("article");
+
+// `document.querySelector` may return null if the selector doesn't match anything.
+if (article) {
+  const text = article.textContent;
+  const wordMatchRegExp = /[^\s]+/g; // Regular expression
+  const words = text.matchAll(wordMatchRegExp);
+  // matchAll returns an iterator, convert to array to get word count
+  const wordCount = [...words].length;
+  const readingTime = Math.round(wordCount / 200);
+  const badge = document.createElement("p");
+  // Use the same styling as the publish information in an article's header
+  badge.classList.add("color-secondary-text", "type--caption");
+  badge.textContent = `⏱️ ${readingTime} min read`;
+  badge.style.color = "red";
+
+  // Support for API reference docs
+  const heading = article.querySelector("h1");
+  // Support for article docs with date
+  const date = article.querySelector("time")?.parentNode;
+
+  (date ?? heading).insertAdjacentElement("afterend", badge);
+}
+
+
+
+// const paragraphs = document.getElementsByTagName('p');
+// console.log(paragraphs); // Returns an HTMLCollection of all <p> elements
+
+// Array.from(paragraphs).forEach(paragraph => {
+//     paragraph.style.color = 'red';
+// })
